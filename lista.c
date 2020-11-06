45dc5c455aa539d41f494096902330aba80a7e72
Juan <jbiancuzzo@fi.uba.ar>
2020-11-06 14:23:25 -0300

    Agregando un iterador externo e interno
    El iterador externo tiene las funciones de crear, destruir, comprobar el
    siguiente, avanzar y devolver el elemento actual, nada muy raro
    El iterador interno recorre toda la lista haciendo una funcion que se
    pasa por parametro y devuelve la cantidad de elementos que recorrió

--- lista.c
+++ lista.c
@@ -305,23 +305,66 @@ void lista_destruir(lista_t* lista) {
 }
 
 lista_iterador_t* lista_iterador_crear(lista_t* lista) {
-    return NULL;
+    if (!lista)
+        return NULL;
+
+    lista_iterador_t* iterador = malloc(sizeof(lista_iterador_t));
+
+    if (!iterador)
+        return NULL;
+
+    iterador->lista = lista;
+    iterador->corriente = lista->nodo_inicio;
+
+    return iterador;
 }
 
 bool lista_iterador_tiene_siguiente(lista_iterador_t* iterador) {
-    return true;
+    if (!iterador)
+        return false;
+
+    return iterador->corriente->siguiente != NULL;
 }
 
 bool lista_iterador_avanzar(lista_iterador_t* iterador) {
+    if (!iterador)
+        return false;
+
+    if (!lista_iterador_tiene_siguiente(iterador))
+        return false;
+
+    iterador->corriente = iterador->corriente->siguiente;
     return true;
 }
 
 void* lista_iterador_elemento_actual(lista_iterador_t* iterador) {
-    return NULL;
+    if (!iterador)
+        return NULL;
+
+    if (!iterador->corriente)
+        return NULL;
+
+    return iterador->corriente->elemento;
 }
 
-void lista_iterador_destruir(lista_iterador_t* iterador) {}
+void lista_iterador_destruir(lista_iterador_t* iterador) {
+    free(iterador);
+}
 
 size_t lista_con_cada_elemento(lista_t* lista, bool (*funcion)(void*, void*), void *contexto) {
-    return 0;
+    if (!lista)
+        return 0;
+
+    nodo_t* nodo = lista->nodo_inicio;
+    size_t contador = 0;
+
+    while (contador < lista->cantidad) {
+
+        funcion(nodo, contexto);
+
+        nodo = nodo->siguiente;
+        contador++;
+    }
+
+    return contador;
 }
